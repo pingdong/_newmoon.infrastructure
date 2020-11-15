7e16a6b53e8548ae52434ee62144b046bb4a3d5d
Ping Dong <ping.dong@live.com>
2020-11-15 13:46:52 +1300

    Output resorce group name

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,12 @@
-output "name" {
+output "rg-integration_test_name" {
   # Only output integration test for cleaning up
   value = var.target == "integration_test" ? module.rg-integration_test[0].name : ""
+}
+
+output "rg-compute_name" {
+  value = var.target == "integration_test" ? "" : module.rg-data.name
+}
+
+output "rg-data_name" {
+  value = var.target == "integration_test" ? "" : module.rg-data.name
 }
