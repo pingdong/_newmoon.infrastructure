cd95156be551720e07ad19b1a51527c0fea9c349
Ping Dong <ping.dong@live.com>
2020-11-16 20:28:10 +1300

    Bug fixed

--- terraform/modules/func_app/outputs.tf
+++ terraform/modules/func_app/outputs.tf
@@ -2,6 +2,7 @@ output "name" {
   value = azurerm_function_app.current.name
 }
 
+# TODO: Return slot name list
 output "slot_name" {
-  value = azurerm_function_app_slot.current[0].name
+  value = length(var.slots) == 0 ? "" : azurerm_function_app_slot.current[0].name
 }
