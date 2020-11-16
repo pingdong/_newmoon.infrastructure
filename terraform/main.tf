3f7b483ef75a5cf21cd1bcde478b6d6d45660089
Ping Dong <ping.dong@live.com>
2020-11-16 23:08:21 +1300

    No slot in dev

--- terraform/main.tf
+++ terraform/main.tf
@@ -261,7 +261,7 @@ module "func" {
                                 # Keyvault is not required in the integration testing
                                 value = var.target == local.target.general ? replace("@Microsoft.KeyVault(SecretUri=<id>)", "<id>", azurerm_key_vault_secret.app_configuration[0].id) : data.azurerm_app_configuration.integration_test[0].primary_read_key.0.connection_string
                               }]
-  slots                     = local.functions[count.index].slots
+  slots                     = var.target == local.target.general ? local.functions[count.index].slots : []
 
   depends_on                = [
                                 module.rg-compute,
