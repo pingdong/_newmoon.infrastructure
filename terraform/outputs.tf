36942685915bc02c3af4c641d60fb002b7392278
Ping Dong <ping.dong@live.com>
2020-11-16 20:55:04 +1300

    Bug fixed

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,36 +1,22 @@
+// TODO: Don't show output if not create
+
 output "rg-integration_test_name" {
   # Only output integration test for cleaning up
-  # value = var.target == "integration_test" ? module.rg-integration_test[0].name : ""
-  value = module.rg-integration_test[0].name 
-
-  depends_on = [
-    module.rg-integration_test
-  ]
+  value = var.target == "integration_test" ? module.rg-integration_test[0].name : ""
 }
 
 output "rg-compute_name" {
-  # value = var.target == "integration_test" ? "" : module.rg-compute[0].name
-  value = module.rg-compute[0].name
-
-  depends_on = [ 
-    module.rg-compute
-  ]
+  value = var.target == "integration_test" ? "" : module.rg-compute[0].name
 }
 
 output "rg-data_name" {
-  # value = var.target == "integration_test" ? "" : module.rg-data[0].name
-  value = module.rg-data[0].name
-
-  depends_on = [ 
-    module.rg-data
-  ]
+  value = var.target == "integration_test" ? "" : module.rg-data[0].name
 }
 
 output "func-venue-name" {
   value = module.func[0].name
 }
 
-// TODO: Don't show slot_name output if not create slot
 output "func-venue-slot_name" {
   value = module.func[0].slot_name
 }
