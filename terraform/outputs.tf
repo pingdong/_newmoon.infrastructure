36c5f19ed5b615201d51b9e041b658c9e88bb6cb
Ping Dong <ping.dong@live.com>
2020-11-23 20:56:47 +1300

    Bug fixed

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,21 +2,21 @@
 
 output "rg-integration_test_name" {
   # Only output integration test for cleaning up
-  value = substr(var.target, 0, 16) == "integration_test" ? module.rg-integration_test[0].name : ""
+  value = var.target == "integration_test" ? module.rg-integration_test[0].name : ""
 }
 
 output "rg-compute_name" {
-  value = substr(var.target, 0, 16) == "integration_test" ? "" : module.rg-compute[0].name
+  value = var.target == "general" ? module.rg-compute[0].name : ""
 }
 
 output "rg-data_name" {
-  value = substr(var.target, 0, 16) == "integration_test" ? "" : module.rg-data[0].name
+  value = var.target == "general" ? module.rg-data[0].name : ""
 }
 
 output "func-venue-name" {
-  value = module.func[0].name
+  value = var.target == "general" || contains(var.integration_testing.features, 'venue')  ? module.func[0].name : ""
 }
 
 output "func-venue-slot_name" {
-  value = module.func[0].slot_name
+  value = var.target == "general" || (contains(var.integration_testing.features, 'venue') && var.environment == "prod" ) ? module.func[0].slot_name : ""
 }
